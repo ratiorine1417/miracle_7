03a6c773ef47b5642fcb8fc4845b9540942879e8
backduu <power4290@gmail.com>
2025-07-31 18:02:28 +0900

    크롤링 받을 준비 완료

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from ui.dashboard import show_homepage
 from ui.sidebar.sidebar import init_sidebar
+from scraping import crawling
 
 st.set_page_config(
     page_title="7번방의 기적",
@@ -12,13 +13,9 @@ st.set_page_config(
 #사용자 입력값 사이드바로부터 받기
 selected_location, deposit_range, rent_range = init_sidebar()
 
-#필터링 적용
-filtered_df = df[
-    (df['지역'] == selected_location) &
-    (df['보증금'] >= deposit_range[0]) & (df['보증금'] <= deposit_range[1]) &
-    (df['월세'] >= rent_range[0]) & (df['월세'] <= rent_range[1])
-]
-
+# 필터링 적용
+filtered_df = crawling(selected_location, deposit_range[1], deposit_range[0], rent_range[1], rent_range[0])
+print("크롤링데이터" + filtered_df)
 st.title("🏡 7번방의 기적")
 st.write("선택한 지역:", selected_location)
 st.write("보증금 범위:", deposit_range)
